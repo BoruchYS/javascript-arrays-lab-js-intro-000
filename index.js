e87c49157e73af7ab832c9d2eca554998d097146
barry.c.schechter@gmail.com <barry.c.schechter@gmail.com>
2018-09-06 23:47:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ function destructivelyRemoveFirstKitten(name){
 function appendKitten(name){
   var kittens_new = kittens;
   kittens_new.push(name)
+  return kittens_new
 }
 function prependKitten(name){
   var kittens_new = kittens;
   kittens.unshift(name)
+  return kittens_new
 }
